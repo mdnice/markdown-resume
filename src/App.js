d11424a62c08b588f4404a3af756c89e1b95fac9
Galahad <itchat@protonmail.com>
2022-11-23 12:50:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 }
 
 const contentWidth = 210 - 11 * 2;
-const contentHeight = 296 - 16 * 2;
+const contentHeight = 296 * 2 - 16 * 2;
 
 const styles = theme => ({
   app: {
