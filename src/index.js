fc5659f21f31f3e2642897ec86fcba0d748ae546
Galahad <itchat@protonmail.com>
2022-11-26 18:15:43 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import hint from "./store/hint";
 import dialog from "./store/dialog";
 
 import { Provider } from "mobx-react";
-import url('https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@100;300;400;500;700;900&display=swap');
+import url('https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Noto+Sans+SC:wght@100;300;400;500;700;900&display=swap');
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
